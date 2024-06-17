a82462435f6f67b6eceb478414776cf80f82d974
StjepanCecura <stjepan.cecura777@gmail.com>
2024-06-18 01:35:35 +0200

    Added database context
    Added database context and registered it

---
+++ KingICTAkademijaSC/Models/AkademijaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KingICTAkademijaSC.Models
+{
+    public class AkademijaContext : DbContext
+    {
+        public AkademijaContext(DbContextOptions<AkademijaContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Proizvod> Proizvodi { get; set; } = null!;
+        public DbSet<Korisnik> Korisnici { get; set; } = null!;
+    }
+}
--- KingICTAkademijaSC/Program.cs
+++ KingICTAkademijaSC/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using KingICTAkademijaSC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AkademijaContext>(opt =>
+    opt.UseInMemoryDatabase("AkademijaIMDB"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
